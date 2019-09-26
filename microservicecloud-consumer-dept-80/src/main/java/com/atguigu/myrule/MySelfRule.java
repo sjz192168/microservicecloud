4bec0965585ccb91eef3db85daaba1c76e22e267
sjz192168 <shijz1014@163.com>
2019-09-26 15:13:52 +0800

    自定义ribbon软负载均衡算法每个服务调用五次

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class MySelfRule {
     @Bean
     public IRule myRule(){
-        return new RandomRule();
+        return new RandomRule_ZY();
     }
 }
