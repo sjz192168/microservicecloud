a6550e194ab30751dd117d67edd9e1e6b7670757
sjz192168 <shijz1014@163.com>
2019-10-08 16:13:56 +0800

    修改Hystrix主启动类

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_Hystrix_App.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_Hystrix_App.java
@@ -2,12 +2,16 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后将自动注册进eureka服务中
-public class DeptProvider8001_App {
+@EnableDiscoveryClient//服务发现
+@EnableCircuitBreaker//对hystrix熔断机制的支持
+public class DeptProvider8001_Hystrix_App {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(DeptProvider8001_Hystrix_App.class,args);
     }
 }
