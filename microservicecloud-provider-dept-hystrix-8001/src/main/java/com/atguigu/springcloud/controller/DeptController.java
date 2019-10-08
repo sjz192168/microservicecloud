da8ecd6ce514487c4e0008e2aeb734da1177d627
sjz192168 <shijz1014@163.com>
2019-10-08 16:14:35 +0800

    新增熔断器注解和异常正确返回方法

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +18,25 @@ public class DeptController {
         return deptService.add(dept);
     }
 
+    /**
+     * 在查询数据库没有对应的信息时，会抛出异常，会自动调用@HystrixCommand(fallbackMethod = "processHystrix_Get")
+     * 标注好的指定的方法
+     * @param id
+     * @return
+     */
     @RequestMapping(value="/dept/get/{id}",method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
     public Dept add(@PathVariable("id") Long id){
-        return deptService.get(id);
+        Dept dept = this.deptService.get(id);
+        if (null==dept){
+            throw new RuntimeException("该ID: "+id+"没有对应的信息，null--@HystrixCommand");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id")Long id){
+        return new Dept().setDeptno(id).setDname("该ID: "+id+"没有对应的信息，null--@HystrixCommand").setDb_source("no this database in mysql");
+
     }
 
     @RequestMapping(value="/dept/list",method = RequestMethod.GET)
