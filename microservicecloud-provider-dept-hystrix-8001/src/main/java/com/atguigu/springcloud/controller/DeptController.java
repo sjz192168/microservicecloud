17bbdb503d70e44d454a5c5718c4b14baa52885c
sjz192168 <shijz1014@163.com>
2019-10-09 10:46:13 +0800

    修改方法名

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -26,7 +26,7 @@ public class DeptController {
      */
     @RequestMapping(value="/dept/get/{id}",method = RequestMethod.GET)
     @HystrixCommand(fallbackMethod = "processHystrix_Get")
-    public Dept add(@PathVariable("id") Long id){
+    public Dept get(@PathVariable("id") Long id){
         Dept dept = this.deptService.get(id);
         if (null==dept){
             throw new RuntimeException("该ID: "+id+"没有对应的信息，null--@HystrixCommand");
